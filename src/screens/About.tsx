5c6ad86084ec61dad55d7cfff2be5b0509174656
Ranjitm1212 <ranjitm1212@outlook.com>
2026-02-27 11:29:04 +0530

    aboutus

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -70,7 +70,7 @@ const About: React.FC<Props> = ({ isDark, toggleTheme }) => {
     <SafeAreaView style={[styles.container, { backgroundColor: colors.background }]}>
       <CustomHeader
         title="About Us"
-         isDark={isDark}
+        isDark={isDark}
         rightComponent={
           <TouchableOpacity onPress={toggleTheme}>
             <Text style={{ fontSize: 20 }}>
@@ -79,10 +79,23 @@ const About: React.FC<Props> = ({ isDark, toggleTheme }) => {
           </TouchableOpacity>
         }
       />
-
-      <Text style={{ color: colors.text }}>
-        This is the About screen.
-      </Text>
+      <View style={styles.aboutBody}>
+         <Text style={[, styles.abouttext, { color: colors.text }]}>
+          Shangrila Industries Pvt. Ltd is a WHO-GMP and Schedule-M compliant pharmaceutical manufacturer driven by a commitment to quality, affordability, and
+          ethical healthcare. Located in the pollution-free region of Sikkim, our two modern manufacturing units
+          operate with advanced automation, stringent quality systems, and a skilled workforce dedicated to delivering safe and effective medicines.
+        </Text>
+        <Text style={[, styles.abouttext, { color: colors.text }]}>
+          We offer a wide range of dosage forms including tablets, capsules, oral liquids, and external preparations. Our manufacturing excellence, combined with a
+          zero-compromise approach toward ethics and quality, has enabled us to become a reliable partner for both domestic and global markets. With a customer-centric
+          mindset and a focus on continuous improvement, we aim to support healthcare professionals, institutions, and third-party brands with dependable, value-driven solutions.
+        </Text>
+         <Text style={[, styles.abouttext, { color: colors.text }]}>
+          Our units receive 10 years of excise benefits from the Sikkim government, allowing us to provide cost-effective solutions without compromising on quality. At Shangrila
+          Pharma, quality is our foundation, ethics are our strength, and patient well-being is our purpose. We continue to evolve with a shared mission of contributing to a
+          healthier, and a more compassionate future.
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -94,4 +107,14 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
+  abouttext: {
+    color: "black",
+    fontSize: 16,
+  fontFamily: "roboto",
+  margin:5
+  },
+  aboutBody:{
+margin:1,
+padding:10
+  }
 });
